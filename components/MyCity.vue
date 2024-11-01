d97c96f618c2f2da86d30424af14d4a331c011ed
Quentin Heinis <quentin@heinis.dev>
2024-11-01 18:55:38 +0100

    feat(city) : add circle in bg

--- components/MyCity.vue
+++ components/MyCity.vue
@@ -1,14 +1,161 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import gsap from "gsap";
+
+onMounted(() => {
+  let circle1 = [
+    {
+      borderWidth: "40px",
+      size: "75%",
+    },
+    {
+      borderWidth: "0px",
+      size: "90%",
+    },
+    {
+      borderWidth: "8px",
+      size: "95%",
+    },
+    {
+      borderWidth: "8px",
+      size: "0%",
+    },
+  ];
+
+  let circle2 = [
+    {
+      borderWidth: "20px",
+      size: "60%",
+    },
+    {
+      borderWidth: "5px",
+      size: "80%",
+    },
+    {
+      borderWidth: "2px",
+      size: "0%",
+    },
+  ];
+
+  gsap.to(".circles__effect.-e1", {
+    "--effectsize": circle1[0].size,
+    "--borderwidth": circle1[0].borderWidth,
+    duration: 0.55,
+  });
+  gsap.to(".circles__effect.-e1", {
+    "--effectsize": circle1[1].size,
+    "--borderwidth": circle1[1].borderWidth,
+    duration: 0.5,
+    delay: 0.55,
+  });
+  gsap.to(".circles__effect.-e2", {
+    "--effectsize2": circle2[0].size,
+    "--borderwidth2": circle2[0].borderWidth,
+    duration: 0.5,
+    delay: 0.55,
+  });
+  gsap.to(".circles__effect.-e2", {
+    "--effectsize2": circle2[1].size,
+    "--borderwidth2": circle2[1].borderWidth,
+    duration: 0.5,
+    delay: 1,
+  });
+  gsap.to(".circles__effect.-e2", {
+    "--effectsize2": circle2[2].size,
+    "--borderwidth2": circle2[2].borderWidth,
+    duration: 0.5,
+    delay: 1.6,
+  });
+  gsap.to(".circles__effect.-e1", {
+    "--effectsize": circle1[2].size,
+    "--borderwidth": circle1[2].borderWidth,
+    duration: 0.1,
+    delay: 1.5,
+  });
+  gsap.to(".circles__effect.-e1", {
+    "--effectsize": circle1[3].size,
+    "--borderwidth": circle1[3].borderWidth,
+    duration: 1,
+    delay: 1.6,
+  });
+
+  // gsap.to(circle1, {
+  //   duration: 1,
+  //   cssRule: {
+  //     borderWidth: "5px",
+  //   },
+  // });
+});
+</script>
 
 <template>
-  <div class="img">
-    <img src="/tokyo.png" alt="" />
-    <div class="fog"></div>
-    <div class="fog2"></div>
+  <div class="city">
+    <div class="circles">
+      <span class="circles__main" />
+      <span class="circles__effect -e1" />
+      <span class="circles__effect -e2" />
+    </div>
+    <div class="img">
+      <img src="/tokyo.png" alt="" />
+      <div class="fog"></div>
+      <div class="fog2"></div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.city {
+  position: relative;
+}
+.circles {
+  --size: 250px;
+  &__main {
+    width: var(--size);
+    height: var(--size);
+    background-color: #d12f2d;
+    border-radius: 50%;
+    position: absolute;
+    top: 10%;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  &__effect {
+    width: var(--size);
+    height: var(--size);
+    border-radius: 50%;
+    position: absolute;
+    top: 10%;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    &::after {
+      content: "";
+      width: 75%;
+      height: 75%;
+      border: 20px solid #fff;
+      border-radius: 50%;
+    }
+    &.-e1 {
+      --effectsize: 0%;
+      --borderwidth: 8px;
+      &::after {
+        width: var(--effectsize);
+        height: var(--effectsize);
+        border-width: var(--borderwidth);
+      }
+    }
+    &.-e2 {
+      --effectsize2: 0%;
+      --borderwidth2: 8px;
+      &::after {
+        width: var(--effectsize2);
+        height: var(--effectsize2);
+        border-width: var(--borderwidth2);
+      }
+    }
+  }
+}
 .img {
   position: relative;
   width: 100%;
