5311e499f948ceefa58ff0f9e9e1b97ee777d219
Quentin Heinis <quentin@heinis.dev>
2024-11-03 13:27:29 +0100

    add SEO

--- app.vue
+++ app.vue
@@ -1,3 +1,55 @@
+<script setup lang="ts">
+useHead({
+  title: "Tokyo - Quentin Heinis",
+  htmlAttrs: {
+    lang: "en",
+  },
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon.png",
+    },
+  ],
+  meta: [
+    {
+      name: "description",
+      content:
+        "Découvrez Tokyo et le Mont Fuji au travers de cette expérience visuel.",
+    },
+    { name: "author", content: "Quentin Heinis" },
+    { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+
+    { property: "og:title", content: "Tokyo - Quentin Heinis" },
+    { property: "og:type", content: "website" },
+    { property: "og:url", content: "https://www.tokyo.heinis.dev" },
+
+    { property: "og:image", content: "/TokyoFinal.png" },
+    { property: "og:image:width", content: "1200" },
+    { property: "og:image:height", content: "630" },
+    { property: "og:image:alt", content: "Tokyo" },
+
+    {
+      property: "og:description",
+      content:
+        "Découvrez Tokyo et le Mont Fuji au travers de cette expérience visuel.",
+    },
+    { property: "og:site_name", content: "Tokyo - Quentin Heinis" },
+    { property: "og:locale", content: "en_US" },
+
+    { property: "twitter:card", content: "summary_large_image" },
+    { property: "twitter:url", content: "https://www.tokyo.heinis.dev" },
+    { property: "twitter:title", content: "Tokyo - Quentin Heinis" },
+    {
+      property: "twitter:description",
+      content:
+        "Découvrez Tokyo et le Mont Fuji au travers de cette expérience visuel.",
+    },
+    { property: "twitter:image", content: "/TokyoFinal.png" },
+  ],
+});
+</script>
+
 <template>
   <div class="wrapper">
     <Tokyo />
